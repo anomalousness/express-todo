5a7aab79bd023a8a77095ddbd43830503f3a44f8
Pablo <pablito@me.com>
2024-03-11 11:49:12 +0000

    Added logs for visibility.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const app = require('./app');
 const port = process.env.PORT || 3000;
 
+console.log(`DB_USERNAME: ${process.env.DB_USERNAME}`);
+console.log(`DB_PASSWORD: ${process.env.DB_PASSWORD}`);
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
